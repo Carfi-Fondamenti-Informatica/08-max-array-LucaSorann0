c05451b6bbf0b566ed44e8f15d1954f73d0b8806
LucaSorann0 <118919272+LucaSorann0@users.noreply.github.com>
2022-12-12 19:38:08 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
+#include "lib.h"
 
+int maggiore (int dimension, int alfa[]) {
+int p=1, q=0;
+if(alfa[q]<alfa[p]) {
+for (p; p<dimension-1; p++){
+while (alfa[q]<=alfa[p]) {
+q++;
+if(q==dimension){
+return alfa[p];
+}}}
+return alfa[p];
+}
+if (alfa[p]<alfa[q]) {
+for (q; q<dimension-1; q++) {
+while (alfa[p]<=alfa[q]) {
+p++;
+if (p==dimension) {
+return alfa[q];
+}}}
+return alfa[q];
+}}
