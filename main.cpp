114d660dd3aad47c0254849cdeac6af05340b829
LucaSorann0 <118919272+LucaSorann0@users.noreply.github.com>
2022-12-12 19:39:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int dimension = 0;
+cin >> dimension;
+int beta[dimension];
+for (int i=0; i<dimension; i++) {
+beta[i]=0;
+} for (int i=0; i<dimension; i++) {
+cin >> beta[i];
+}
+cout << maggiore (dimension, beta);
   return 0;
 }
